8acc1ad2cda0c09faab21d53b292f4c9cb23c837
Michael Kubín <michael.kubn@gmail.com>
2023-06-15 21:19:54 +0200

    add: .env

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { App } = require("@slack/bolt");
+require('dotenv').config()
 
 const app = new App({
-  token: "xoxb-3499222920212-5433864155763-zwsXpGJnpLxwwlyzPjJka2Fx",
-  signingSecret: "e01d64ca0c19ad3219cc3367855837a9",
+  token: process.env.SLACK_TOKE,
+  signingSecret: process.env.SLACK_SECRET,
   socketMode: true,
-  appToken:
-    "xapp-1-A05CUC3L944-5434066261235-d41a092d60363d2dbb60e39e2d9b1cb91251d976e2e9bf13ddbc9f5f0fdd3c47",
+  appToken: process.env.SLACK_APP_TOKEN,
 });
 
 const prompts = [
@@ -15,13 +15,13 @@ const prompts = [
   "Nebyl jsem na meeting, za to může",
   "Nemam schválené zadání, za to může",
   "Vývovj nedodal, za to může",
-  "Mac client nefunguje s outlook, říkal jsem to, za to může"
+  "Mac client nefunguje s outlook, říkal jsem to, za to může",
 ];
 
 app.command("/bullshit", async ({ command, ack, say }) => {
   try {
     await ack();
-    say(`${prompts[(Math.floor(Math.random() * prompts.length))]} :vk:`);
+    say(`${prompts[Math.floor(Math.random() * prompts.length)]} :vk:`);
   } catch (err) {
     console.log("err");
     console.error(err);
@@ -31,7 +31,7 @@ app.command("/bullshit", async ({ command, ack, say }) => {
 app.command("/apple", async ({ command, ack, say }) => {
   try {
     await ack();
-    say('Musím si koupit Vision PRO. :man-heart-man:');
+    say("Musím si koupit Vision PRO. :man-heart-man:");
   } catch (err) {
     console.log("err");
     console.error(err);
@@ -41,7 +41,7 @@ app.command("/apple", async ({ command, ack, say }) => {
 app.command("/brno", async ({ command, ack, say }) => {
   try {
     await ack();
-    say('Co to je? Za to beztak může :vk: :brno:');
+    say("Co to je? Za to beztak může :vk: :brno:");
   } catch (err) {
     console.log("err");
     console.error(err);
